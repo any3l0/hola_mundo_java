fdfef0a79296def779f50dae5ce7cfbba3017fc7
anye3l0 <anyelohack@gmail.com>
2023-03-29 19:41:14 +0200

    añadidos comentarios del autor

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,10 @@
+/**
+* Muestra la frase Holamundo por pantalla 
+*
+* autor Angel M. Aragón
+*
+*/
+
 public class HolaMundo {
 	public static void main (String[] args) {
 		System.out.println("Holamundo");
